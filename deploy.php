dec8f66f44c03d8f8ff0d21e9b85e643853f6d29
JFValLop <jvaldezl4@miumg.edu.gt>
2026-05-17 10:57:54 -0600

    fix: deploy.php con git correcto

--- deploy.php
+++ deploy.php
@@ -7,5 +7,9 @@ if (!hash_equals('sha256=' . hash_hmac('sha256', $payload, $secret), $header)) {
     http_response_code(403); exit;
 }
 
-$output = shell_exec('cd /home/ixcanula/public_html && git pull origin master 2>&1');
-echo $output;
+$git_dir   = '/home1/ixcanula/home/ixcanula/public_html/.git';
+$work_tree = '/home1/ixcanula/public_html';
+
+$fetch  = shell_exec("git --git-dir=$git_dir --work-tree=$work_tree fetch origin master 2>&1");
+$reset  = shell_exec("git --git-dir=$git_dir --work-tree=$work_tree reset --hard origin/master 2>&1");
+echo $fetch . $reset;
