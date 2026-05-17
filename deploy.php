2432aca0e45a1ec4004830c4d7687d44466c231b
JFValLop <jvaldezl4@miumg.edu.gt>
2026-05-17 09:35:41 -0600

    Add deploy webhook

--- deploy.php
+++ deploy.php
@@ -7,5 +7,5 @@ if (!hash_equals('sha256=' . hash_hmac('sha256', $payload, $secret), $header)) {
     http_response_code(403); exit;
 }
 
-$output = shell_exec('cd /home/wixbalco/public_html && git pull origin master 2>&1');
+$output = shell_exec('cd /home/ixcanula/public_html && git pull origin master 2>&1');
 echo $output;
